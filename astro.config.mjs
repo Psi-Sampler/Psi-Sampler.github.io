9d46ba4cc4184693932a3117e72a79cff6cb8564
Taehoon Yoon <taehoon@kaist.ac.kr>
2025-06-04 13:22:45 +0900

    ..

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://stochsync.github.io',
+  site: 'https://psi-sampler.github.io/',
   vite: {
       server: {
           watch: {
